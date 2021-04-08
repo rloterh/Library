b24f15f709e111fdce4a077b4fa5f1724a46746d
Robert Loterh <rloterh@gmail.com>
2021-04-08 20:57:51 +0100

    Refactor code

--- app.js
+++ app.js
@@ -33,18 +33,19 @@ class UI {
     const div  = document.createElement('div');
     div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
-    if (el.classList.contains('delete')) {
+    // if (el.classList.contains('delete')) {
       const container =  document.querySelector('.container');
       const table =  document.querySelector('.table');
       container.insertBefore(div, table);
       setTimeout(( )=> document.querySelector('.alert').remove(), 3000);
-    } else if (el.classList.contains('submit')) { 
-      const container = document.querySelector('.modal-body');
-      const form = document.querySelector('#book-form');
-      container.insertBefore(div, form);
-      setTimeout(( )=> document.querySelector('.alert').remove(), 3000);
-    }
-  }
+    } 
+    // else if (el.classList.contains('submit')) { 
+    //   const modalBody = document.querySelector('.modal-body');
+    //   const form = document.querySelector('#book-form');
+    //   modalBody.insertBefore(div, form);
+    //   setTimeout(( )=> document.querySelector('.alert').remove(), 3000);
+    // }
+
   static toggleRead(readBtn) {
     readBtn.textContent = readBtn.textContent === 'Read' ? 'Unread' : 'Read';
   }
@@ -52,7 +53,7 @@ class UI {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#pages').value = '';
-    document.querySelector('#read').value = '';
+    document.querySelector('#read').value = 'Have You Read It?';
   }
 }
 
@@ -94,13 +95,13 @@ class Store {
 
 document.addEventListener('DOMContentLoaded', UI.displayLibrary);
 
-document.querySelector('#book-form').addEventListener('click', (e) => {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
   const read = document.querySelector('#read').value;
-  if (title === '' || author === '' || pages === '' || read === 'Have you read it?') {
+  if (title === '' || author === '' || pages === '' || read === 'Have You Read It?') {
     UI.showAlert('please fill in all fields', 'danger');
   } else {
     const book = new Book(title, author, pages, read);
@@ -115,7 +116,7 @@ document.querySelector('#book-list').addEventListener('click',(e) =>{
   if (e.target.classList.contains('delete')) {
     UI.deleteBook(e.target);
     Store.removeBook(e.target.parentElement.parentElement.firstElementChild.textContent);
-    UI.showAlert(e.target, 'Book Removed', 'success');
+    UI.showAlert('Book Removed', 'success');
   } else if (e.target.classList.contains('read-toggle')) {
     UI.toggleRead(e.target);
     Store.updateRead(e.target.parentElement.parentElement.firstElementChild.textContent);
