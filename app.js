fec3fdd4bdbb572ec94bf1403dc70848d6a9bcd5
Robert Loterh <rloterh@gmail.com>
2021-04-08 22:10:58 +0100

    Clean code

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 class Book {
-  constructor(title, author, pages, read ) {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages
+    this.pages = pages;
     this.read = read;
+  }
+}
+
+class Store {
+  static getLibrary() {
+    let library;
+    if (localStorage.getItem('library') === null) {
+      library = [];
+    } else {
+      library = JSON.parse(localStorage.getItem('library'));
+    }
+    return library;
+  }
+
+  static addBook(book) {
+    const library = Store.getLibrary();
+    library.push(book);
+    localStorage.setItem('library', JSON.stringify(library));
+  }
+
+  static updateRead(title) {
+    const library = Store.getLibrary();
+    library.forEach((book, index) => {
+      if (book.title === title) {
+        const readSwitch = library[index].read === 'Read' ? 'Unread' : 'Read';
+        library[index].read = readSwitch;
+      }
+    });
+    localStorage.setItem('library', JSON.stringify(library));
+  }
+
+  static removeBook(title) {
+    const library = Store.getLibrary();
+    library.forEach((book, index) => {
+      if (book.title === title) {
+        library.splice(index, 1);
+      }
+    });
+    localStorage.setItem('library', JSON.stringify(library));
   }
 }
 
@@ -12,10 +51,11 @@ class UI {
     const library = Store.getLibrary();
     library.forEach((book) => UI.addBookToLibrary(book));
   }
+
   static addBookToLibrary(book) {
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
-    row.innerHTML =`
+    row.innerHTML = `
     <td style="vertical-align:middle;">${book.title}</td>
     <td style="vertical-align:middle;">${book.author}</td>
     <td style="vertical-align:middle;">${book.pages}</td>
@@ -24,72 +64,39 @@ class UI {
     `;
     list.appendChild(row);
   }
+
   static deleteBook(el) {
-    if(el.classList.contains('delete')){
+    if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
+
   static showAlert(el, message, className) {
-    const div  = document.createElement('div');
+    const div = document.createElement('div');
     div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
     if (el.classList.contains('delete')) {
-      const container =  document.querySelector('.container');
-      const table =  document.querySelector('.table');
+      const container = document.querySelector('.container');
+      const table = document.querySelector('.table');
       container.insertBefore(div, table);
-      setTimeout(( )=> document.querySelector('.alert').remove(), 3000);
-    } else if (el.classList.contains('submit')) { 
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    } else if (el.classList.contains('submit')) {
       const modalBody = document.querySelector('.modal-body');
       const form = document.querySelector('#book-form');
       modalBody.insertBefore(div, form);
-      setTimeout(( )=> document.querySelector('.alert').remove(), 3000);
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
   }
 
   static toggleRead(readBtn) {
     readBtn.textContent = readBtn.textContent === 'Read' ? 'Unread' : 'Read';
   }
-  static clearFields(){
+
+  static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#pages').value = '';
     document.querySelector('#read').value = 'Have You Read It?';
-  }
-}
-
-class Store {
-  static getLibrary (){
-    let library;
-    if(localStorage.getItem('library') === null) {
-      library = [];
-    } else {
-      library = JSON.parse(localStorage.getItem('library'));
-    }
-    return library;
-  }
-  static addBook(book) {
-    const library = Store.getLibrary();
-    library.push(book);
-    localStorage.setItem('library', JSON.stringify(library));
-  }
-  static updateRead(title) {
-    const library = Store.getLibrary();
-    library.forEach((book, index) => {
-      if(book.title === title){
-        const readSwitch = library[index].read === 'Read' ? 'Unread' : 'Read';
-        library[index].read = readSwitch;
-      }
-    });
-    localStorage.setItem('library', JSON.stringify(library));
-  }
-  static removeBook(title) {
-    const library = Store.getLibrary();
-    library.forEach((book, index) => {
-      if(book.title === title){
-        library.splice(index, 1);
-      }
-    });
-    localStorage.setItem('library', JSON.stringify(library));
   }
 }
 
@@ -107,16 +114,16 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const book = new Book(title, author, pages, read);
     UI.addBookToLibrary(book);
     Store.addBook(book);
-    UI.showAlert(e.target.lastElementChild,'Book Added', 'success');
+    UI.showAlert(e.target.lastElementChild, 'Book Added', 'success');
     UI.clearFields();
   }
 });
 
-document.querySelector('#book-list').addEventListener('click',(e) =>{
+document.querySelector('#book-list').addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
     UI.deleteBook(e.target);
     Store.removeBook(e.target.parentElement.parentElement.firstElementChild.textContent);
-    UI.showAlert(e.target,'Book Removed', 'success');
+    UI.showAlert(e.target, 'Book Removed', 'success');
   } else if (e.target.classList.contains('read-toggle')) {
     UI.toggleRead(e.target);
     Store.updateRead(e.target.parentElement.parentElement.firstElementChild.textContent);
